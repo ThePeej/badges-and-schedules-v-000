cef52751dc5b3f26587fa3ccc19cae17a6d36857
PJ Frias <peejfrias@gmail.com>
2018-01-03 23:09:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,12 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end
 
-def batch_badge_creator
-
+def batch_badge_creator(speakers)
+  badges = []
+  speakers.each do |name|
+    badges << "Hello, my name is #{name}"
+  end
+  badges
 end
 
 def assign_rooms
